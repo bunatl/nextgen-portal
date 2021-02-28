e426af58dc567c7d1993e3d22634aa0a0f06e3ba
bunatl <lukas.bunat@gmail.com>
2021-02-28 23:51:31 +0100

    updated AWS lambda functions

--- aws-server/lambda/login.js
+++ aws-server/lambda/login.js
@@ -2,32 +2,32 @@ const { searchUser } = require('./dbOperations');
 
 exports.handler = async (event) => {
     if (!event.username || !event.psswd)
-        return response(200, "Incorrect parameters.");
+        return response(200, "Incorrect parameters.", false);
 
     // body params
     const username = event.username;
     const psswd = event.psswd;
-    console.log("params", username, psswd);
 
     try {
         // check if users exists
         const resSearch = await searchUser(username);
         if (resSearch.Item === undefined)
-            return response(200, `User with username ${ username } doesn't exists.`);
+            return response(200, `User with username ${ username } doesn't exists.`, false);
 
         return (username === resSearch.Item.username && psswd === resSearch.Item.password)
-            ? response(200, `Successfull login.`)
-            : response(200, `Username or password does not match.`);
+            ? response(200, `Successfull login.`, true)
+            : response(200, `Username or password does not match.`, false);
     } catch (err) {
         console.log("err", err);
-        return response(200, err);
+        return response(200, err, false);
     }
 };
 
-const response = (code, data) => {
+const response = (code, data, logged) => {
     console.log(code, " ", data);
     return {
         statusCode: code,
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        logged
     };
 };
--- aws-server/lambda/register.js
+++ aws-server/lambda/register.js
@@ -5,36 +5,34 @@ const {
 
 exports.handler = async (event) => {
     if (!event.username || !event.psswd || !event.email)
-        return response(200, "Incorrect parameters.");
+        return response(200, "Incorrect parameters.", false);
 
     // body params
     const username = event.username;
     const psswd = event.psswd;
     const email = event.email;
-    console.log("params", username, psswd, email);
 
     try {
         // check if users exists
         const resSearch = await searchUser(username);
         if (resSearch.Item !== undefined)
-            return response(200, `User with username ${ username } already exists.`);
-        console.log("continue");
+            return response(200, `User with username ${ username } already exists.`, false);
         // insert user
         await addUser(username, psswd, email);
         // check if added succesfully
         const resSearch2 = await searchUser(username);
         return resSearch2.Item === undefined
-            ? response(200, "User couldn't be registered. Please try again.")
-            : response(201, `User with username ${ username } has been succesfully created.`);
+            ? response(200, "User couldn't be registered. Please try again.", false)
+            : response(200, `User with username ${ username } has been succesfully created.`, true);
     } catch (err) {
-        return response(200, err);
+        return response(200, err, false);
     }
 };
 
-const response = (code, data) => {
-    console.log(code, " ", data);
+const response = (code, data, registered) => {
     return {
         statusCode: code,
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        registered
     };
 };
